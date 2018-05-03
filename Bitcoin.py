bfa9c7e0404a29c6453cfca849d52dd35ae9c375
Motti Marom <motti.marom@gmail.com>
2018-05-03 01:21:21 -0500

    no message

--- Bitcoin.py
+++ Bitcoin.py
@@ -11,20 +11,23 @@ with open('config.json') as json_data:
     data = json.load(json_data)
     num_of_bitcoins = int(data["number_of_bitcoins"])
 
-
 driver = webdriver.Chrome()
 driver.get("http://preev.com")
+
+previous_usd_value = -1
+
 try:
     bc_value_field = driver.find_element_by_id("numField")
+    print("The value of " + str(num_of_bitcoins) + " Bitcoin(s) in USD is: ")
+    while True:
+        time.sleep(1)
+        current_usd_value = bc_value_field.get_attribute("value").replace(",", "")
+        if previous_usd_value != current_usd_value:
+            total_bit_value = num_of_bitcoins * int(current_usd_value)
+            sys.stdout.write('\r' + "$" + str(total_bit_value))
 
 except NoSuchElementException:
     print("Unable to find the bitcoin field")
     driver.quit()
 
-print("The value of " + str(num_of_bitcoins) + " Bitcoin(s) in USD is: ")
 
-while True:
-    time.sleep(1)
-    usd_value = bc_value_field.get_attribute("value").replace(",", "")
-    total_bit_value = num_of_bitcoins * int(usd_value)
-    sys.stdout.write('\r' + "$" + str(total_bit_value))
