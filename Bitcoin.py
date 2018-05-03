3f0f587684355ab1e4ff7f2c30608cbfc0b405c8
Motti Marom <motti.marom@gmail.com>
2018-05-03 00:59:13 -0500

    no message

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,17 +1,30 @@
 __author__ = 'mmarom'
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 import time
 import sys
 
+import json
+
+with open('config.json') as json_data:
+    data = json.load(json_data)
+    num_of_bitcoins = int(data["number_of_bitcoins"])
+
+
 driver = webdriver.Chrome()
 driver.get("http://preev.com")
-usd_value = driver.find_element_by_id("numField")
+try:
+    bc_value_field = driver.find_element_by_id("numField")
 
-num_of_bitcoins = input("Please enter the number of bitcoins: ")
+except NoSuchElementException:
+    print("Unable to find the bitcoin field")
+    driver.quit()
 
-print("The value of " + num_of_bitcoins + " Bitcoin(s) in USD is:")
+print("The value of " + str(num_of_bitcoins) + " Bitcoin(s) in USD is: ")
+
 while True:
     time.sleep(1)
-    total_bit_value = int(num_of_bitcoins) * int(usd_value.get_attribute("value").replace(",", ""))
-    sys.stdout.write('\r' + str(total_bit_value))
+    usd_value = bc_value_field.get_attribute("value").replace(",", "")
+    total_bit_value = num_of_bitcoins * int(usd_value)
+    sys.stdout.write('\r' + "$" + str(total_bit_value))
